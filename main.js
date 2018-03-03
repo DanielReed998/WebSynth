0cf05a43d64a91ec5495ca537e34f1910cad26a1
Daniel Reed <daniel.reed998@gmail.com>
2018-03-03 14:09:16 -0600

    sequelize error, disabled db for now sinceince its just client side

--- main.js
+++ main.js
@@ -1,11 +1,18 @@
 'use strict'; 
 
-const {db} = require('./server/db/models')
+// const {db} = require('./server/db/models')
 const app = require('./server')
 const PORT = 8080;
 
-db.sync() // if you update your db schemas, make sure you drop the tables first and then recreate them
-.then(() => {
-  console.log('db synced')
-  app.listen(PORT, () => console.log(`listening on port ${PORT}`))
-});
+app.listen(PORT, () => console.log(`listening on port ${PORT}`))
+
+/*
+** Below will be added once backend functionality is up and running.
+*/
+
+// db.sync() // if you update your db schemas, make sure you drop the tables first and then recreate them
+// .then(() => {
+//   console.log('db synced')
+//   app.listen(PORT, () => console.log(`listening on port ${PORT}`))
+// });
+
