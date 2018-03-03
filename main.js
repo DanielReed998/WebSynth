203799a94dba22b14b3b74cf9964da121d89bdfe
Daniel Reed <daniel.reed998@gmail.com>
2018-03-03 16:49:52 -0600

    fixed port issue

--- main.js
+++ main.js
@@ -2,9 +2,8 @@
 
 // const {db} = require('./server/db/models')
 const app = require('./server')
-const PORT = 8080;
 
-app.listen(PORT, () => console.log(`listening on port ${PORT}`))
+app.listen(process.env.PORT || 8080, () => console.log(`listening on port ${PORT}`))
 
 /*
 ** Below will be added once backend functionality is up and running.
